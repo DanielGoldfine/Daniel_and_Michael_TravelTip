9725963600df9ab10845e63a915f98c7457b7e64
Your Name <daniel720pgmail.com>
2020-04-19 16:33:09 +0300

    added map

---
+++ js/travel-tip-main.js
@@ -0,0 +1,48 @@
+
+
+var gTempMarkerData;
+
+var gLocations = [];
+
+function initMap(lat = 29.558244, lng = 34.955198) {
+
+    var elMap = document.querySelector('.map');
+    var options = {
+        center: { lat, lng },
+        zoom: 15
+    };
+
+    var map = new google.maps.Map(
+        elMap,
+        options
+    );
+
+    var marker = new google.maps.Marker({
+        position: { lat, lng },
+        map,
+        title: 'Marker'
+    });
+    map.addListener('click', function(position) {
+        // alert(position.latLng)
+        onAddMarker(position.latLng, map);
+    });
+}
+
+function onAddMarker(latLng, map) {
+
+    gLocations.push(latLng);
+    console.log(latLng)
+
+    var marker = new google.maps.Marker({
+        pos: latLng,
+        map: map
+    });
+}
+
+function onUserLocation() {
+    navigator.geolocation.getCurrentPosition(goToUserLocation)
+}
+
+function goToUserLocation(position) {
+    initMap(position.coords.latitude, position.coords.longitude)
+}
